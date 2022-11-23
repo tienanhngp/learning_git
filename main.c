d47a7812d91322a6167164a35f6cac24b38d210b
tienanhngp <70998983+tienanhngp@users.noreply.github.com>
2022-11-23 21:55:37 +0700

    Update main.c
    Tien Anh

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int add(int a, int b) {
-
+    return a + b;
 }
 
 int sub(int a, int b) {
-
+    return a - b;
 }
 
 int mul(int a, int b) {
-
+    return a*b;
 }
 
 int div(int a, int b) {
-
+    return a/b;
 }
 
 int main() {
